8fe1ba27dbd2b3cef01961dab4f8206b0d7e58dc
doomsterinc <doomsterinc1@gmail.com>
2016-08-20 02:19:09 -0300

    addChanges3

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let audioContext = new AudioContext();
 
+function startLoop(audioBuffer) {
+  let sourceNode = audioContext.createBufferSource();
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.loopStart = 2.98;
+  sourceNode.loopEnd = 3.80;
+  sourceNode.connect(audioContext.destination);
+  sourceNode.start(0, 2.98);
+}
+
 fetch('itsgonnarain.mp3')
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
-    let sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = audioBuffer;
-    sourceNode.connect(audioContext.destination);
-    sourceNode.start();
+    startLoop(audioBuffer);
+    startLoop(audioBuffer);
   })
   .catch(e => console.error(e));
-

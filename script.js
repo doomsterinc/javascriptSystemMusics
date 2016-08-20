4056135a5487f3279ea932dc34850cd5e0dcfec6
doomsterinc <doomsterinc1@gmail.com>
2016-08-20 02:20:19 -0300

    addChanges5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let audioContext = new AudioContext();
 
-function startLoop(audioBuffer, pan = 0) {
+function startLoop(audioBuffer, pan = 0, rate = 1) {
   let sourceNode = audioContext.createBufferSource();
   let pannerNode = audioContext.createStereoPanner();
 
@@ -8,6 +8,7 @@ function startLoop(audioBuffer, pan = 0) {
   sourceNode.loop = true;
   sourceNode.loopStart = 2.98;
   sourceNode.loopEnd = 3.80;
+  sourceNode.playbackRate.value = rate;
   pannerNode.pan.value = pan;
 
   sourceNode.connect(pannerNode);
@@ -21,6 +22,6 @@ fetch('itsgonnarain.mp3')
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
     startLoop(audioBuffer, -1);
-    startLoop(audioBuffer, 1);
+    startLoop(audioBuffer, 1, 1.002);
   })
   .catch(e => console.error(e));
